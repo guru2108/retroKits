701a8e9ae155edfeaf022f92e0a41262ab5d6ffc
Guru <72003730+guru2108@users.noreply.github.com>
2024-03-10 14:40:47 +0000

    Update Providers.tsx

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
             mode="payment"
             cartMode="client-only"
             stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-            successUrl="http://localhost:3000/stripe/success"
-            cancelUrl="https://commerce-next-yt.vercel.app/stripe/error"
+            successUrl="https://retro-kits-git-main-gurus-projects-00aafc45.vercel.app/stripe/success"
+            cancelUrl="https://retro-kits-git-main-gurus-projects-00aafc45.vercel.app/stripe/error"
             currency="GBP"
             billingAddressCollection={false}
             shouldPersist={true}
