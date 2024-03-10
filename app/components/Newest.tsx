9dfd5efbd93010a6d1311bf647e1eeba86b8cc96
Guru <72003730+guru2108@users.noreply.github.com>
2024-03-10 23:22:00 +0000

    Update Newest.tsx

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -44,6 +44,7 @@ export default async function Newest() {
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {data.map((product) => (
                         <div key={product._id} className="group relative">
+                            <Link href={`/product/${product.slug}`}>
                             <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
                                 <Image
                                     src={product.imageUrl}
@@ -69,6 +70,7 @@ export default async function Newest() {
                                     {product.price} GBP
                                 </p>
                             </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
