ef3210af8c34377261e5f1ada8978c49c36bd33a
Guru <72003730+guru2108@users.noreply.github.com>
2024-03-10 15:39:14 +0000

    Update page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -78,7 +78,7 @@ export default async function ProductPge({
                         <div className="mb-4">
                             <div className="flex items-end gap-2">
                                 <span className="text-xl font-bold text-white md:text-2xl">
-                                    {data.price}
+                                    {data.price} GBP
                                 </span>
                             </div>
 
